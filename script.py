065d8d3875589883fd5ab13d795565042322db82
rivermont <william.11bennett@gmail.com>
2018-05-08 14:06:42 -0400

    Added psuedocode outline.

--- script.py
+++ script.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python3
+
+# import requests
+# from random import randint
+
+# Sides of the square bounding box, in kilometers
+size = 1
+
+# Set this to False if you want to get areas without content
+# This will mostly be empty ocean.
+verify_exists = True
+
+
+def main():
+    pass
+# Loop:
+
+# Get random point
+
+# Make bounding box
+
+# Send request
+# response = requests.get()
+
+if verify_exists:
+    # Check to see if there is anything in the returned area
+    # possibly using arbitrary file size?
+    pass
+
+# End loop
+
+# Save content to .osm file
+
+
+if __name__ == "__main__":
+    main()
+    print("Done!)
+
